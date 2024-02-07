95e64a5b7fb6551139f0e1dc5c7acc2a41f35237
lyes07 <83643064+lyes07@users.noreply.github.com>
2024-02-08 00:04:50 +0100

    create a script to fill the db table

---
+++ Backend/filltable.py
@@ -0,0 +1,12 @@
+f = open("./Backend/frinsert.txt", "a")
+t = open("./Backend/numbers.txt", "r")
+i=0
+
+for line in t:
+    i+=1
+    f.write("insert into frnum(id,audio,word) values("+str(i)+",audio,\'"+str(line.removesuffix("\n"))+"\');\n")
+    
+t.close()
+f.close()
+
+
