c8151121486346bbf656680da8e21134ebb37b4b
lyes07 <83643064+lyes07@users.noreply.github.com>
2024-02-08 00:03:52 +0100

    create the postgres database and tables

--- Backend/tables.sql
+++ Backend/tables.sql
@@ -1,5 +1,17 @@
-create table numbers(
+CREATE ROLE french WITH LOGIN PASSWORD 'password';/* create user and give it a password */
+
+GRANT CONNECT ON DATABASE random_db TO french; /* just to login */
+
+ALTER ROLE french CREATEDB; /* the user can create his own database */
+
+create database numbers; /* create the database */
+
+\c numbers   /* connect into the database */
+
+create table frnum(  /* create the french numbers table */
     id integer primary key not null,
-    audio varchar(50) not null,
-    word varchar(50) not null
+    audio varchar(150) not null,
+    word varchar(100) not null
 );
+
+insert into frnum values(,'','');
