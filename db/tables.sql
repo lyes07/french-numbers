351a5cc87d17008445d4c19b4449b188287cf4e1
lyes07 <83643064+lyes07@users.noreply.github.com>
2024-02-10 12:45:22 +0100

    database setup in postgres

--- db/tables.sql
+++ db/tables.sql
@@ -1,12 +1,27 @@
+/* install postgresql and login to the admin user ex: postgres */
+
 CREATE ROLE french WITH LOGIN PASSWORD 'password';/* create user and give it a password */
 
 GRANT CONNECT ON DATABASE random_db TO french; /* just to login */
 
 ALTER ROLE french CREATEDB; /* the user can create his own database */
 
+exit /* exit the current user */
+
+psql -U french -h localhost -d random_db -p your_port /* connect to the new user */
+
 create database numbers; /* create the database */
 
-\c numbers   /* connect into the database */
+exit /* exit the french user */
+
+psql -U postgres  /* connect to the postgres or any user with admin rights */
+
+REVOKE CONNECT ON DATABASE random_db FROM french; /* just to login */
+
+exit /* exit the Admin user for the last time */
+
+psql -U french -h localhost -d numbers -p your_port /*port ex:5432*/ /* connect into the database */
+  
 
 create table frnum(  /* create the french numbers table */
     id integer primary key not null,
@@ -14,4 +29,5 @@ create table frnum(  /* create the french numbers table */
     word varchar(100) not null
 );
 
+/* fill the french numbers frnum table */
 insert into frnum values( ,'','');
