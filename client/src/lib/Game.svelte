3f1cfe0f40280ed4709ce949d5117d2bebe58f10
lyes07 <83643064+lyes07@users.noreply.github.com>
2024-03-28 00:53:21 +0100

    game logic done

--- client/src/lib/Game.svelte
+++ client/src/lib/Game.svelte
@@ -1,15 +1,16 @@
 <script>
-  // so see if the game implementaion is finished
-  let ready = true;
-
   import { onMount } from "svelte";
   import Api from "./Api";
-
   import Loading from "./Loading.svelte";
-  import Error from "./Error.svelte";
 
   let list = [];
+  let numbers = [];
+  let words = [];
   let gridData = [];
+  let selectedBox = null;
+  let matchedBoxes = [];
+  let gameOver = false;
+
   async function getListOfRandomNumbers() {
     let isError = false;
     try {
@@ -25,23 +26,14 @@
       console.error(err.message);
     } finally {
       if (!isError) {
-        let numbers = [];
-        let words = [];
-
         for (let i = 0; i < list.length; i++) {
           numbers[i] = list[i].id;
           words[i] = list[i].word;
         }
-        gridData = shuffle([...shuffle(numbers), ...shuffle(words)]);
+        let tempNumbers = [...numbers];
+        let tempWords = [...words];
+        gridData = shuffle([...shuffle(tempNumbers), ...shuffle(tempWords)]);
       }
-    }
-  }
-
-  function playAudio() {
-    const audioElement = document.getElementById("myAudio");
-    if (audioElement) {
-      // @ts-ignore
-      audioElement.play();
     }
   }
 
@@ -69,32 +61,131 @@
     grid.push(Array(4).fill(false));
   }
 
+  function isItAMatch(firstIndex, secondIndex) {
+    let boxes = document.querySelectorAll(".box");
+    // @ts-ignore
+    let first = boxes[firstIndex].childNodes[0].data;
+    // @ts-ignore
+    let second = boxes[secondIndex].childNodes[0].data;
+
+    // the checking logic
+
+    let number;
+    let word;
+
+    if (!isNaN(+first)) {
+      number = first;
+      word = second;
+    } else {
+      number = second;
+      word = first;
+    }
+
+    let numberIndex;
+    let wordIndex;
+
+    let len = 6; // numbers.length    or    words.length
+
+    for (let i = 0; i < len; i++) {
+      if (number == numbers[i]) {
+        numberIndex = i;
+        break;
+      }
+    }
+    for (let i = 0; i < len; i++) {
+      if (word == words[i]) {
+        wordIndex = i;
+        break;
+      }
+    }
+
+    return numberIndex == wordIndex;
+  }
+
+  function disableCorrectBoxes(firstIndex, secondIndex) {
+    let boxes = document.querySelectorAll(".box");
+    // @ts-ignore
+    let first = boxes[firstIndex];
+    // @ts-ignore
+    let second = boxes[secondIndex];
+
+    // @ts-ignore
+    first.style.visibility = "hidden";
+    // @ts-ignore
+    second.style.visibility = "hidden";
+    // @ts-ignore
+    first.style.opacity = "0";
+    // @ts-ignore
+    second.style.opacity = "0";
+  }
+
   function handleClick(row, column) {
-    grid[row][column] = !grid[row][column];
+    if (grid[row][column] === false && selectedBox !== null) {
+      if (
+        isItAMatch(row * 4 + column, (selectedBox >> 2) * 4 + (selectedBox & 3))
+      ) {
+        matchedBoxes.push(row * 4 + column);
+        matchedBoxes.push(selectedBox);
+        console.log(matchedBoxes);
+        disableCorrectBoxes(
+          row * 4 + column,
+          (selectedBox >> 2) * 4 + (selectedBox & 3)
+        );
+        selectedBox = null;
+        if (matchedBoxes.length === 12) {
+          gameOver = true;
+        }
+      } else {
+        setTimeout(() => {
+          grid[selectedBox >> 2][selectedBox & 3] = false;
+          grid[row][column] = false;
+          selectedBox = null;
+        }, 1000);
+      }
+    } else if (grid[row][column] === false && selectedBox === null) {
+      selectedBox = row * 4 + column;
+      grid[row][column] = true;
+    } else if (grid[row][column] === true && selectedBox === row * 4 + column) {
+      grid[row][column] = false;
+      selectedBox = null;
+    }
+  }
+
+  function isBoxDisabled(index) {
+    return matchedBoxes.includes(index);
+  }
+
+  function resetGame() {
+    window.location.reload();
   }
 </script>
 
 <div class="container">
   {#if list === undefined}
     <Loading />
-  {:else if !ready}
-    <Error />
-    <h1 class="middle">Comming Soon</h1>
   {:else}
     <div class="grid">
       {#each grid as row, rowIndex}
         {#each row as box, columnIndex}
-          <button
-            class="box {box ? 'clicked' : ''}"
-            on:click={() => handleClick(rowIndex, columnIndex)}
-            aria-pressed={box}
-          >
-            {gridData[rowIndex * 4 + columnIndex]}
-            <!-- {rowIndex}, {columnIndex} -->
-          </button>
+          {#if !isBoxDisabled(rowIndex * 4 + columnIndex)}
+            <button
+              class="box {box ? 'clicked' : ''}"
+              on:click={() => handleClick(rowIndex, columnIndex)}
+              aria-pressed={box}
+            >
+              {gridData[rowIndex * 4 + columnIndex]}
+            </button>
+          {:else}
+            <button class="box" disabled></button>
+          {/if}
         {/each}
       {/each}
     </div>
+    {#if gameOver}
+      <div class="middle">
+        <button class="reload" on:click={resetGame}>Replay?</button>
+      </div>
+    {/if}
   {/if}
 </div>
 
@@ -131,5 +222,19 @@
   .middle {
     display: flex;
     justify-content: center;
+  }
+
+  .reload {
+    border: 2px solid black;
+    border-radius: 0.5rem;
+    padding: 14px 28px;
+    cursor: pointer;
+    border-color: #e7e7e7;
+  }
+
+  .reload:hover {
+    transition: 0.5s;
+    background: #e7e7e7;
+    color: black;
   }
 </style>
